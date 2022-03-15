cbc2dad3ae6d8077514d2b08a9dfd067140f7797
Mick <93855183+Mick-2097@users.noreply.github.com>
2022-03-15 11:48:36 +1100

    Add files via upload

--- script.js
+++ script.js
@@ -1,107 +1,33 @@
-let q1 = document.querySelector('.question-1')
-let q2 = document.querySelector('.question-2')
-let q3 = document.querySelector('.question-3')
-let q4 = document.querySelector('.question-4')
-let q5 = document.querySelector('.question-5')
-let a1 = document.querySelector('.answer-1')
-let a2 = document.querySelector('.answer-2')
-let a3 = document.querySelector('.answer-3')
-let a4 = document.querySelector('.answer-4')
-let a5 = document.querySelector('.answer-5')
-let arrow = document.querySelector('.arrow')
+let questions = document.querySelectorAll('.question')
+let answers = document.querySelectorAll('.answer')
+let arrows = document.querySelectorAll('.arrow')
+let questionArea = document.querySelector('.questions')
 
-q1.addEventListener('click', event => {
-    if (a1.style.display === 'block') {
-        q1.style.fontWeight = 'normal'
-        a1.style.display = 'none'
+questionArea.addEventListener('click', event => {
+    if (!event.target.classList.contains('question')) return
+    if (event.target.style.fontWeight === 'bold') {
+        event.target.style.fontWeight = 'normal'
+        event.target.nextElementSibling.style.display = 'none'
         event.target.querySelector('img').classList.remove('flip')
     }else {
-        q1.style.fontWeight = 'bold'
-        q2.style.fontWeight = 'normal'
-        q3.style.fontWeight = 'normal'
-        q4.style.fontWeight = 'normal'
-        q5.style.fontWeight = 'normal'
-        a1.style.display = 'block'
-        a2.style.display = 'none'
-        a3.style.display = 'none'
-        a4.style.display = 'none'
-        a5.style.display = 'none'
+        questions.forEach(x => x.style.fontWeight = 'normal')
+        answers.forEach(x => x.style.display = 'none')
+        arrows.forEach(x => x.classList.remove('flip'))
+        event.target.style.color = 'var(--text)'
+        event.target.style.fontWeight = 'bold'
+        event.target.nextElementSibling.style.display = 'block'
         event.target.querySelector('img').classList.add('flip')
     }
 })
-q2.addEventListener('click', event => {
-    if (a2.style.display === 'block') {
-        q2.style.fontWeight = 'normal'
-        a2.style.display = 'none'
-        event.target.querySelector('img').classList.remove('flip')
-    }else {
-        q1.style.fontWeight = 'normal'
-        q2.style.fontWeight = 'bold'
-        q3.style.fontWeight = 'normal'
-        q4.style.fontWeight = 'normal'
-        q5.style.fontWeight = 'normal'
-        a1.style.display = 'none'
-        a2.style.display = 'block'
-        a3.style.display = 'none'
-        a4.style.display = 'none'
-        a5.style.display = 'none'
-        event.target.querySelector('img').classList.add('flip')
+questionArea.addEventListener('mouseover', event => {
+    if (event.target.classList.contains('question') 
+        && event.target.style.fontWeight === 'normal') {
+        event.target.style.color = 'var(--text-hover)'
     }
 })
-q3.addEventListener('click', event => {
-    if (a3.style.display === 'block') {
-        q3.style.fontWeight = 'normal'
-        a3.style.display = 'none'
-        event.target.querySelector('img').classList.remove('flip')
-    }else {
-        q1.style.fontWeight = 'normal'
-        q2.style.fontWeight = 'normal'
-        q3.style.fontWeight = 'bold'
-        q4.style.fontWeight = 'normal'
-        q5.style.fontWeight = 'normal'
-        a1.style.display = 'none'
-        a2.style.display = 'none'
-        a3.style.display = 'block'
-        a4.style.display = 'none'
-        a5.style.display = 'none'
-        event.target.querySelector('img').classList.add('flip')
-    }
-})
-q4.addEventListener('click', event => {
-    if (a4.style.display === 'block') {
-        q4.style.fontWeight = 'normal'
-        a4.style.display = 'none'
-        event.target.querySelector('img').classList.remove('flip')
-    }else {
-        q1.style.fontWeight = 'normal'
-        q2.style.fontWeight = 'normal'
-        q3.style.fontWeight = 'normal'
-        q4.style.fontWeight = 'bold'
-        q5.style.fontWeight = 'normal'
-        a1.style.display = 'none'
-        a2.style.display = 'none'
-        a3.style.display = 'none'
-        a4.style.display = 'block'
-        a5.style.display = 'none'
-        event.target.querySelector('img').classList.add('flip')
-    }
-})
-q5.addEventListener('click', event => {
-    if (a5.style.display === 'block') {
-        q5.style.fontWeight = 'normal'
-        a5.style.display = 'none'
-        event.target.querySelector('img').classList.remove('flip')
-    }else {
-        q1.style.fontWeight = 'normal'
-        q2.style.fontWeight = 'normal'
-        q3.style.fontWeight = 'normal'
-        q4.style.fontWeight = 'normal'
-        q5.style.fontWeight = 'bold'
-        a1.style.display = 'none'
-        a2.style.display = 'none'
-        a3.style.display = 'none'
-        a4.style.display = 'none'
-        a5.style.display = 'block'
-        event.target.querySelector('img').classList.add('flip')
+questionArea.addEventListener('mouseout', event => {
+    if (event.target.classList.contains('question') 
+        && event.target.style.fontWeight === 'normal') {
+        event.target.style.color = 'var(--text)'
     }
 })
